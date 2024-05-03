e465a2eb8595d3f99f7e3efd1945d7b89bf90daf
Tobbb <tobias_Be@hotmail.com>
2024-05-03 21:21:43 +0200

    Update a4988.cpp

--- esphome/components/a4988/a4988.cpp
+++ esphome/components/a4988/a4988.cpp
@@ -28,7 +28,7 @@ void A4988::dump_config() {
 void A4988::loop() {
   bool at_target = this->has_reached_target();
   if (this->sleep_pin_ != nullptr) {
-    bool is_zero = this->current_position == 0;
+    bool is_zero = this->is_current_pos_zero();
     bool sleep_rising_edge = !sleep_pin_state_ & !at_target;
     this->sleep_pin_->digital_write(!at_target && !is_zero);
     this->sleep_pin_state_ = !at_target && !is_zero;
